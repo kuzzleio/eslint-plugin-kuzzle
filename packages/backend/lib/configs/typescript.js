465fdc8d0e7f0df06c0690110b47ef9dc6b06108
Sébastien Rodriguez <sebtiz13@gmail.com>
2023-08-02 10:36:54 +0200

    chore: error level in string format instead number

--- packages/backend/lib/configs/typescript.js
+++ packages/backend/lib/configs/typescript.js
@@ -1,19 +1,19 @@
 module.exports = {
-  rules: {
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "@typescript-eslint/ban-ts-comment": 0,
-    "@typescript-eslint/type-annotation-spacing": 1,
-    "@typescript-eslint/member-delimiter-style": 1,
-    // Fix for https://github.com/typescript-eslint/typescript-eslint/issues/2483
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": "error"
-  },
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/type-annotation-spacing': 'warning',
+    '@typescript-eslint/member-delimiter-style': 'warning',
+    // Fix for https://github.com/typescript-eslint/typescript-eslint/issues/2483
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+  },
 };
