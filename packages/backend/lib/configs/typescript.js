3992e173301494713b98ee3eec99cd4406094cb0
Sébastien Rodriguez <sebtiz13@gmail.com>
2023-08-02 10:57:09 +0200

    feat(typescript): add project configuration
    some typescript rules require the path to the project tsconfig files

--- packages/backend/lib/configs/typescript.js
+++ packages/backend/lib/configs/typescript.js
@@ -6,6 +6,9 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['**/tsconfig.json', '**/tsconfig.*.json'],
+  },
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
