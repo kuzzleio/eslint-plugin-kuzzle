0da74841836018d1a205c6410c22a3503f2ce80c
Florian Maunier <fmaunier@kuzzle.io>
2024-12-09 17:50:39 +0100

    chore(config): update own eslint config, prepare for typescript

---
+++ packages/backend/eslint.config.js
@@ -0,0 +1,30 @@
+import eslint from '@eslint/js';
+import eslintPlugin from 'eslint-plugin-eslint-plugin';
+import nodePlugin from 'eslint-plugin-n';
+import prettierPluginRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  nodePlugin.configs['flat/recommended'],
+  eslintPlugin.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.nodeBuiltin,
+        ...globals.es2023,
+      },
+    },
+  },
+  {
+    files: ['tests/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+  },
+  prettierPluginRecommended,
+);
